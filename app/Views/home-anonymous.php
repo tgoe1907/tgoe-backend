c7a1b879acdf1b98ba4ec2b5fe60f862791ce3c0
Timo Rupp <timo.rupp@axonivy.com>
2024-05-31 22:08:38 +0200

    Fix: Remove useless login form from error screen (in case session is not logged in)

--- app/Views/home-anonymous.php
+++ app/Views/home-anonymous.php
@@ -5,24 +5,5 @@
 </div>
 
 <div class="col-md-6">
-<div class="card card-primary">
-  <div class="card-header">
-    <h3 class="card-title">Benutzeranmeldung</h3>
-  </div>
-  <form method='POST' action='/'>
-    <div class="card-body">
-      <div class="form-group">
-        <label for="loginform-usr">E-Mail</label>
-        <input type="text" class="form-control" id="loginform-usr" name="loginform-usr" placeholder="Benutzername eingeben">
-      </div>
-      <div class="form-group">
-        <label for="loginform-pwd">Passwort</label>
-        <input type="password" class="form-control" name='loginform-pwd' id="loginform-pwd" placeholder="Passwort eingeben">
-      </div>
-    </div>
-    <div class="card-footer">
-      <button type="submit" class="btn btn-outline-primary btn-block"><i class="fa fa-sign-in-alt"></i> anmelden</button>
-    </div>
-  </form>
-</div>
+
 </div>
